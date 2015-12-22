c28cfb6ac9df83656a4bb35cc9dd7b2bd9690306
Joan Climent <joandelason@gmail.com>
2015-12-22 22:42:59 +0100

    desordenado

--- Csv2Json/Csv2json.py
+++ Csv2Json/Csv2json.py
@@ -7,10 +7,21 @@ import json
 with open("hygxyz.csv", "rt") as f:	
 	try:
 	    reader = csv.reader(f)
-	    with open("stars.js", "wa") as output:
-	    	ouput.write("starData = ")
-		    for row in reader:
-		        json.dump(row, output)
+	    # header
+	    header = reader.next()
+	    #sun
+	    reader.next() 
+
+	    rows = iter(reader)
+	    mapping = [header.index(x) for x in header]
+
+	    with open("stars.js", "w+") as output:
+	    	#ouput.write("starData = [\n")
+			for row in rows:
+				a = [row[i] for i in mapping]
+				json.dump(a, output)
+				output.write(",\n");
+			output.write("];");
 	finally:
 	    f.close()
 
